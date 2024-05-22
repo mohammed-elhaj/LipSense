2f78fabeb62c179fa4864e3a514b9e48c57a2886
mohammed-elhaj <78878412+mohammed-elhaj@users.noreply.github.com>
2024-05-22 19:13:48 +0200

    Update modelutil.py

--- app/modelutil.py
+++ app/modelutil.py
@@ -27,8 +27,7 @@ def load_model() -> Sequential:
 
     model.add(Dense(41, kernel_initializer='he_normal', activation='softmax'))
 
-    model.load_weights(os.path.join('models','checkpoint'), by_name=True, skip_mismatch=True)
-
+    model.load_weights(os.path.join('models', 'checkpoint.h5'), by_name=True, skip_mismatch=True)
 
     #model.load_weights(os.path.join('..','models','checkpoint'))
 
