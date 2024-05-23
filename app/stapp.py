605ed422d9ff3d10ed247af9224e3eec30ff93be
Osmanoor <51276385+Osmanoor@users.noreply.github.com>
2024-05-23 17:37:22 +0200

    Update stapp.py

--- app/stapp.py
+++ app/stapp.py
@@ -55,8 +55,8 @@ if options:
     with col2: 
         st.info('This is all the machine learning model sees when making a prediction')
         video, annotations = load_data(tf.convert_to_tensor(file_path))
-        imageio.mimsave('animation.gif', video, fps=10)
-        st.image('animation.gif', width=400) 
+        #imageio.mimsave('animation.gif', video, fps=10)
+        #st.image('animation.gif', width=400) 
 
         st.info('This is the output of the machine learning model as tokens')
         model = load_model()
