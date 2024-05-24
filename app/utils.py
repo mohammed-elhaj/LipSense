88b0c78d3c43069d502d1b402f90fe8207912b94
Osmanoor <51276385+Osmanoor@users.noreply.github.com>
2024-05-24 16:47:12 +0200

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -48,7 +48,7 @@ def load_data(path: str):
     video_path = os.path.join(script_dir, 'data', 's1', f'{file_name}.mpg')
     alignment_path = os.path.join(script_dir, 'data', 'alignments', 's1', f'{file_name}.align')
     
-    frames = load_video(video_path) 
+    frames = load_video(path) 
     alignments = '' #load_alignments(alignment_path)
     
     return frames, alignments
